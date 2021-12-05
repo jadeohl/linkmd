f9ea37933e9520a5b341c87f851ca86f4d6af66f
Jade Ohlhauser <jadeonly@gmail.com>
2021-12-05 12:46:46 -0700

    New icon

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
 				{ from: './src/popup.html' },
 				{ from: './src/icons/icon-64.png' },
 				{ from: './src/icons/icon-128.png' },
+				{ from: './src/icons/icon-512.png' },
 			],
 		}),
 	],
