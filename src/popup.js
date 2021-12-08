7b8b6a53d2772ba9065dc02caa9df0041573b6dc
Jade Ohlhauser <jadeonly@gmail.com>
2021-12-08 09:56:27 -0700

    Separate chrome and mozilla builds

--- src/popup.js
+++ src/popup.js
@@ -1,79 +1,24 @@
-import ToMarkdown from './ToMarkdown.js';
-const md = new ToMarkdown();
+function Popup() {
 
-const closeAfterSeconds = 3;
-
-let activeTab = {
-	title: '',
-	url: '',
-	selectedText: ''
 }
 
-function getSelected() {
-	// This function is passed to the active tab so window means that tab, not this popup
-	return window.getSelection().toString();
-}
-
-async function useSelected(results) {
-	activeTab.selectedText = results[0].result;
-
-	let output = '';
-	if (activeTab.selectedText === '' || activeTab.selectedText === undefined || activeTab.selectedText === null) {
-		output = md.link(activeTab.title, activeTab.url);
-	} else {
-		output = md.quoteLink(activeTab.selectedText, activeTab.title, activeTab.url);
-	}
-
-	try {
-		await navigator.clipboard.writeText(output);
-
-		showResult('result-ok');
-		hidePopup();
-
-	} catch (err) {
-		showResult('result-error');
-		console.error('Failed to copy to clipboard', err);
-	}
-}
-
-async function main() {
-	const queryOptions = { active: true, currentWindow: true };
-	let [tab] = await chrome.tabs.query(queryOptions);
-	activeTab.title = tab.title;
-	activeTab.url = tab.url;
-
-	if (tab.url.substr(0, 4) !== 'http') {
-		showResult('result-error');
-		console.error('http and https only');
-		return;
-	}
-
-	await chrome.scripting.executeScript(
-		{
-			target: { tabId: tab.id },
-			func: getSelected
-		},
-		useSelected
-	);
-}
-
-window.onload = function () {
-	main();
-}
-
-/*
-
- ----------------- Visual
-
- */
-function showResult(id) {
+Popup.prototype.result = function (id) {
 	document.getElementById(id).style.display = 'block';
 }
 
-function hidePopup() {
+Popup.prototype.success = function () {
+	this.result('result-ok');
+	this.hide();
+}
+
+Popup.prototype.hide = function () {
+	const closeAfterSeconds = 3;
+
 	if (closeAfterSeconds > 0) {
 		setTimeout(function () {
 			window.close();
 		}, closeAfterSeconds * 1000);
 	}
 }
+
+module.exports = Popup;
